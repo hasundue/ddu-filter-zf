f7a556057a04c9230307946155d97bedbfdf1c59
denopendabot <denopendabot@github.com>
2023-05-30 00:09:44 +0000

    build(deps): bump ddu_vim from v2.8.4 to v2.9.0

--- denops/@ddu-filters/matcher_zf.ts
+++ denops/@ddu-filters/matcher_zf.ts
@@ -3,7 +3,7 @@ import {
   BaseFilter,
   BaseFilterParams,
   DduItem,
-} from "https://deno.land/x/ddu_vim@v2.8.4/types.ts";
+} from "https://deno.land/x/ddu_vim@v2.9.0/types.ts";
 import { rankToken } from "../../libzf.ts";
 
 interface Params extends BaseFilterParams {
