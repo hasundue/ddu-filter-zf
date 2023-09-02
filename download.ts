b958818b57350bca15c61756eb770afe4d2a1f00
hasundue <hasundue@gmail.com>
2023-09-02 22:07:11 +0900

    feat: `deno task download` to download a pre-built `libzf`

---
+++ download.ts
@@ -0,0 +1,48 @@
+import {
+  dirname,
+  fromFileUrl,
+} from "https://deno.land/std@0.198.0/path/mod.ts";
+import { existsSync } from "https://deno.land/std@0.201.0/fs/mod.ts";
+import { Octokit } from "npm:@octokit/rest@20.0.1";
+
+const dir = dirname(fromFileUrl(import.meta.url));
+
+let filename = "";
+
+switch (Deno.build.os) {
+  case "windows":
+    filename = "zf.dll";
+    break;
+  case "darwin":
+    filename = "libzf.dylib";
+    break;
+  default:
+    filename = "libzf.so";
+    break;
+}
+
+const github = new Octokit().rest.repos;
+
+const release = await github.getLatestRelease({
+  owner: "hasundue",
+  repo: "ddu-filter-zf",
+});
+
+const asset = release.data.assets.find((asset) => asset.name === filename);
+
+if (!asset) {
+  console.error(`Failed to find ${filename} in release.`);
+  Deno.exit(1);
+}
+
+const data = await fetch(asset.browser_download_url).then((res) =>
+  res.arrayBuffer()
+);
+
+if (!existsSync(`${dir}/zf`)) {
+  Deno.mkdirSync(`${dir}/zf`);
+}
+
+Deno.writeFileSync(`${dir}/zf/${filename}`, new Uint8Array(data), {
+  mode: 0o755,
+});
