a0ddbed469f816fd3ba15fb000e5f9e56ed88560
hasundue <hasundue@gmail.com>
2023-09-02 18:18:05 +0900

    build: add support for building for multiple targets

--- build.ts
+++ build.ts
@@ -4,8 +4,8 @@ import $ from "https://deno.land/x/dax@0.34.0/mod.ts";
 // ensure we're in the root of the project
 $.cd(import.meta);
 
+// check for zig and decide which branch to use
 let branch: string;
-
 try {
   const result = await $`zig version`.text();
   branch = result.includes("dev") ? "unstable" : "master";
@@ -25,5 +25,17 @@ console.log(`Updating zf...`);
 await $`git checkout -q ${branch}`;
 await $`git pull -q`;
 
-console.log(`Building libzf...`);
-await $`zig build-lib src/clib.zig -dynamic --name zf`;
+const buildTargetMap = {
+  "linux": "x86_64-linux",
+  "windows": "x86_64-windows",
+  "darwin": "x86_64-macos",
+};
+
+const targets = Deno.args.length > 0
+  ? Deno.args
+  : [buildTargetMap[Deno.build.os as keyof typeof buildTargetMap]];
+
+for (const target of targets) {
+  console.log(`Building libzf for ${target}...`);
+  await $`zig build-lib src/clib.zig -dynamic -target ${target} --name zf`;
+}
