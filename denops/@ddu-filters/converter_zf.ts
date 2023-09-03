20050c21b3ff2cb700acf6f262491d5bb66cdfb0
hasundue <hasundue@gmail.com>
2023-09-03 16:34:01 +0900

    refactor: split matcher and converter

---
+++ denops/@ddu-filters/converter_zf.ts
@@ -0,0 +1,71 @@
+import { basename } from "https://deno.land/std@0.201.0/path/mod.ts";
+import {
+  BaseFilter,
+  BaseFilterParams,
+  DduItem,
+  ItemHighlight,
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { highlightToken } from "../../libzf.ts";
+import { RankTokenParams } from "./matcher_zf.ts";
+
+const SEPARATOR = Deno.build.os === "windows" ? "\\" : "/";
+const HIGHLIGHT_NAME = "zf_matched";
+
+interface Params extends BaseFilterParams {
+  highlight: string;
+}
+
+export class Filter extends BaseFilter<Params> {
+  filter(args: {
+    filterParams: Params;
+    input: string;
+    items: DduItem[];
+  }): DduItem[] {
+    if (!args.input) return args.items;
+
+    const tokens = args.input.trimEnd().split(" ");
+    const strictPath = tokens.length == 1 && args.input.includes(SEPARATOR);
+    const caseSensitive = /[A-Z]/.test(args.input);
+
+    return args.items.map((item) => ({
+      ...item,
+      highlights: highlightItem(item, tokens, {
+        strictPath,
+        caseSensitive,
+        plainText: item.kind !== "file",
+        highlight: args.filterParams.highlight,
+      }),
+    }));
+  }
+  params() {
+    return { highlight: "" };
+  }
+}
+
+function highlightItem(
+  item: DduItem,
+  tokens: string[],
+  params: RankTokenParams & { highlight: string },
+): ItemHighlight[] {
+  const highlights: ItemHighlight[] = [];
+  for (const token of tokens) {
+    const str = item.matcherKey ?? item.word;
+    const indices = highlightToken(
+      str,
+      params.plainText ? null : basename(str),
+      token,
+      params.caseSensitive,
+      params.strictPath,
+    );
+    // TODO: probably better to merge sequential highlights
+    indices.forEach((i) => {
+      highlights.push({
+        name: HIGHLIGHT_NAME,
+        hl_group: params.highlight,
+        col: i + 1,
+        width: 1,
+      });
+    });
+  }
+  return highlights;
+}
