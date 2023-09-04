a38995dac07ab5a337600ae8300cb3664f6703a6
hasundue <hasundue@gmail.com>
2023-09-04 09:43:34 +0900

    refactor(deps): migrate to unknownutil v3.6.0

--- denops/@ddu-filters/sorter_zf.ts
+++ denops/@ddu-filters/sorter_zf.ts
@@ -3,7 +3,9 @@ import {
   BaseFilterParams,
   DduItem,
 } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
-import { isLike } from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
+import { is } from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
+
+const type = { zfRank: is.Number };
 
 export class Filter extends BaseFilter<BaseFilterParams> {
   filter(args: {
@@ -11,8 +13,8 @@ export class Filter extends BaseFilter<BaseFilterParams> {
     items: DduItem[];
   }): DduItem[] {
     return args.items.sort((a, b) => {
-      const ra = isLike({ zfRank: 0 }, a.data) ? a.data.zfRank : 0;
-      const rb = isLike({ zfRank: 0 }, b.data) ? b.data.zfRank : 0;
+      const ra = is.ObjectOf(type)(a.data) ? a.data.zfRank : 0;
+      const rb = is.ObjectOf(type)(b.data) ? b.data.zfRank : 0;
       return ra - rb;
     });
   }
