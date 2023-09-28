233643ab76b01f8f26f5365f39f3b4a8071c00d2
hasundue <hasundue@gmail.com>
2023-09-28 16:06:36 +0900

    test(converter): fix test cases using assertObjectMatch

--- test/converter_test.ts
+++ test/converter_test.ts
@@ -1,4 +1,7 @@
-import { assertObjectMatch } from "https://deno.land/std@0.202.0/assert/mod.ts";
+import {
+  assertEquals,
+  assertObjectMatch,
+} from "https://deno.land/std@0.202.0/assert/mod.ts";
 import { describe, it } from "https://deno.land/std@0.202.0/testing/bdd.ts";
 import { Filter } from "../denops/@ddu-filters/converter_zf.ts";
 
@@ -13,59 +16,51 @@ describe("converter_zf", () => {
 
     const filterParams = { highlight: "Search" };
 
-    // TODO: fix the following tests
-    // ref: https://github.com/denoland/deno_std/issues/3472
+    it("should not highlight when input is empty", () => {
+      const actual = converter.filter({ filterParams, items, input: "" });
+      assertEquals(actual.length, 1);
+      assertObjectMatch(
+        actual[0],
+        { word: "zf/libzf.so" },
+      );
+    });
 
-    // it("should not highlight when input is empty", () => {
-    //   assertObjectMatch(
-    //     converter.filter({ filterParams, items, input: "" }),
-    //     [
-    //       { word: "zf/libzf.so", highlights: [] },
-    //       // deno-lint-ignore no-explicit-any
-    //     ] as any,
-    //   );
-    // });
-
-    // it("should not highlight when input is not matched", () => {
-    //   assertObjectMatch(
-    //     converter.filter({ filterParams, items, input: "a" }),
-    //     [
-    //       { word: "zf/libzf.so", highlights: [] },
-    //       // deno-lint-ignore no-explicit-any
-    //     ] as any,
-    //   );
-    // });
+    it("should not highlight when input is not matched", () => {
+      const actual = converter.filter({ filterParams, items, input: "a" });
+      assertEquals(actual.length, 1);
+      assertObjectMatch(
+        actual[0],
+        { word: "zf/libzf.so", highlights: [] },
+      );
+    });
 
     it("should highlight when input is matched", () => {
+      const actual = converter.filter({ filterParams, items, input: "zf" });
+      assertEquals(actual.length, 1);
       assertObjectMatch(
-        converter.filter({ filterParams, items, input: "zf" }),
-        [
-          {
-            word: "zf/libzf.so",
-            highlights: [
-              { col: 1, name: "zf" },
-              { col: 2, name: "zf" },
-            ],
-          },
-          // deno-lint-ignore no-explicit-any
-        ] as any,
+        actual[0],
+        {
+          word: "zf/libzf.so",
+          highlights: [
+            { col: 1, name: "zf" },
+            { col: 2, name: "zf" },
+          ],
+        },
       );
     });
 
     it("should highlight for strict path", () => {
+      const actual = converter.filter({ filterParams, items, input: "z/l" });
       assertObjectMatch(
-        converter.filter({ filterParams, items, input: "z/l" }),
-        [
-          {
-            word: "zf/libzf.so",
-            highlights: [
-              { col: 1, name: "zf" },
-              { col: 3, name: "zf" },
-              { col: 4, name: "zf" },
-            ],
-          },
-          // deno-lint-ignore no-explicit-any
-        ] as any,
+        actual[0],
+        {
+          word: "zf/libzf.so",
+          highlights: [
+            { col: 1, name: "zf" },
+            { col: 3, name: "zf" },
+            { col: 4, name: "zf" },
+          ],
+        },
       );
     });
   });
