d80a286b921b9d93408f0c6413d04af112545636
denopendabot <denopendabot@github.com>
2023-10-13 00:09:27 +0000

    build(deps): bump std from 0.203.0 to 0.204.0

--- build.ts
+++ build.ts
@@ -1,4 +1,4 @@
-import { existsSync } from "https://deno.land/std@0.203.0/fs/mod.ts";
+import { existsSync } from "https://deno.land/std@0.204.0/fs/mod.ts";
 import $ from "https://deno.land/x/dax@0.35.0/mod.ts";
 
 // ensure we're in the root of the project
--- test/converter_test.ts
+++ test/converter_test.ts
@@ -1,8 +1,8 @@
 import {
   assertEquals,
   assertObjectMatch,
-} from "https://deno.land/std@0.203.0/assert/mod.ts";
-import { describe, it } from "https://deno.land/std@0.203.0/testing/bdd.ts";
+} from "https://deno.land/std@0.204.0/assert/mod.ts";
+import { describe, it } from "https://deno.land/std@0.204.0/testing/bdd.ts";
 import { Filter } from "../denops/@ddu-filters/converter_zf.ts";
 
 describe("converter_zf", () => {
