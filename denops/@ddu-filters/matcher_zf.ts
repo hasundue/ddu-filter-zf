89fe61f5bda418d3012b2a8bcc978ca23a107171
denopendabot <denopendabot@github.com>
2024-04-18 00:10:11 +0000

    build(deps): bump unknownutil from v3.13.0 to v3.18.0

--- denops/@ddu-filters/matcher_zf.ts
+++ denops/@ddu-filters/matcher_zf.ts
@@ -4,7 +4,7 @@ import {
   BaseFilterParams,
   DduItem,
 } from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
-import { is } from "https://deno.land/x/unknownutil@v3.13.0/mod.ts";
+import { is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
 import { rankToken } from "../../libzf.ts";
 
 const SEPARATOR = Deno.build.os === "windows" ? "\\" : "/";
--- denops/@ddu-filters/sorter_zf.ts
+++ denops/@ddu-filters/sorter_zf.ts
@@ -3,7 +3,7 @@ import {
   BaseFilterParams,
   DduItem,
 } from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
-import { is } from "https://deno.land/x/unknownutil@v3.13.0/mod.ts";
+import { is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
 
 const type = { zfRank: is.Number };
 
